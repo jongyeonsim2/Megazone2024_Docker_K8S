73e63656fa7d8f5972672a0e6137feca2d446179
unknown <jongyeonsim2@gmail.com>
2024-09-20 16:17:16 +0900

    1st commit

--- 01_K8S/ch2/factorial-cache-app/src/main/java/com/example/factorialcacheapp/FactorialCacheController.java
+++ 01_K8S/ch2/factorial-cache-app/src/main/java/com/example/factorialcacheapp/FactorialCacheController.java
@@ -1,8 +1,10 @@
 package com.example.factorialcacheapp;
 
+import com.fasterxml.jackson.core.exc.InputCoercionException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,9 +16,24 @@ public class FactorialCacheController {
     @Value("${APP_LANGUAGE}")
     private String language;
 
-    // http://localhost/factorial/10
+    /**
+     * K8S secret 객체 생성 명령문을 참조
+     */
+    @Value("${API_KEY}")
+    private String apiKey;
+
+    // http://localhost/factorial/10&key=123
     @GetMapping("/factorial/{n}")
-    public String calculateFactorial(@PathVariable("n") int number) {
+    public String calculateFactorial(@PathVariable("n") int number,
+                                     @RequestParam(value="key", required=false) String key) {
+
+        if (number > 10) {
+            if (!apiKey.equals(key)) {
+                throw new IncorrectApiKeyException(
+                        "Incorrect API Key: " + apiKey
+                );
+            }
+        }
 
         return switch (language) {
             case "ko" -> number + " 팩토리얼은" + number + "입니다.";
